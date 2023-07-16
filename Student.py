e9ab6fce485e02a9289dac23c13e2f0fec45be1d
Rafail Abdyushev <rafaelab@mail.ru>
2023-07-16 11:20:50 +0300

    class student

--- Student.py
+++ Student.py
@@ -1,6 +1,10 @@
 from OpenFile import OpenFile
+from Range import Range
 
 class Student:
+    firstName = Range()
+    lastName = Range()
+
     def __init__(self, *args):
         self.id = args[0]
         self.firstName = args[1]
@@ -8,11 +12,32 @@ class Student:
         self.gender = args[3]
         self.age = args[4]
 
+    @property
+    def fullName(self):
+        return f'{self.firstName} {self.lastName}'
+
     def __setattr__(self, key, value):
         subjects = ["mathematic", "english", "literature", "hystory"]
         mapping = value if key not in subjects else {'grade': value[0], 'tests': value[1]}
         return object.__setattr__(self, key, mapping)
     
+    def __getattribute__(self, item):
+        return object.__getattribute__(self, item)
+    
     def __repr__(self) -> str:
-        return f'Объект {self.__class__.__name__} {self.firstName} {self.id}'
+        subjects = ["mathematic", "english", "literature", "hystory"]
+        sum, count = 0, 0
+        strStudent = f'Студент {self.fullName} имеет следующую успеваемость:\n'
+        for sub in subjects:
+            if hasattr(self, sub):
+                value = getattr(self, sub)
+                strStudent += f'\tПредмет "{sub}": оценка {value["grade"]}; средняя тестов {value["tests"]}\n'
+                sum += int(value["grade"])
+                count += 1
+
+        strStudent += f'\tСредняя оценка: {sum/count}'
+        return strStudent
+
+
+
 
