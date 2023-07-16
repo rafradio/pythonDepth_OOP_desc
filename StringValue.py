ac97c2cb716255cda8c865f4b070f0f53854ec26
Rafail Abdyushev <rafaelab@mail.ru>
2023-07-16 12:13:51 +0300

    decriptor

---
+++ StringValue.py
@@ -0,0 +1,20 @@
+class StringValue:
+    def __init__(self):
+        pass
+
+    def __set_name__(self, owner, name):
+        self.param_name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.param_name)
+
+    def __set__(self, instance, value):
+        self.validate(value, instance.id)
+        setattr(instance, self.param_name, value)
+
+    def validate(self, value, id):
+        if not value.isalpha():
+            raise TypeError(f'Объект с {id = }: значение "{value}" должно состоять только из букв')
+        
+        if not value[0].isupper():
+            raise TypeError(f'Объект с {id = }: значение "{value}" должно начинаться с заглавной буквы')
